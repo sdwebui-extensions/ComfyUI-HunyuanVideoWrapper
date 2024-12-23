2fe649be41d2ddce62a99d69d1754b51cacb97ab
kijai <40791699+kijai@users.noreply.github.com>
2024-12-23 11:42:20 +0200

    compile fixes

--- enhance_a_video/globals.py
+++ enhance_a_video/globals.py
@@ -1,14 +1,16 @@
+import torch
+
 NUM_FRAMES = None
 FETA_WEIGHT = None
 ENABLE_FETA_SINGLE = False
 ENABLE_FETA_DOUBLE = False
 
-
+@torch.compiler.disable()
 def set_num_frames(num_frames: int):
     global NUM_FRAMES
     NUM_FRAMES = num_frames
 
-
+@torch.compiler.disable()
 def get_num_frames() -> int:
     return NUM_FRAMES
 
@@ -23,16 +25,19 @@ def disable_enhance():
     ENABLE_FETA_SINGLE = False
     ENABLE_FETA_DOUBLE = False
 
+@torch.compiler.disable()
 def is_enhance_enabled_single() -> bool:
     return ENABLE_FETA_SINGLE
 
+@torch.compiler.disable()
 def is_enhance_enabled_double() -> bool:
     return ENABLE_FETA_DOUBLE
 
+@torch.compiler.disable()
 def set_enhance_weight(feta_weight: float):
     global FETA_WEIGHT
     FETA_WEIGHT = feta_weight
 
-
+@torch.compiler.disable()
 def get_enhance_weight() -> float:
     return FETA_WEIGHT
